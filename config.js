9010a7e836fb8b5787c1eb5f01e53b899238494b
juanco091-hub <juanco091@gmail.com>
2026-07-24 15:44:53 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG_ENV = {
+    // Configuración de Colores e Iluminación (Formatos: hex #38bdf8, rgb, color name)
+    COLOR_ILUMINACION_BTN_CEN: "#ca8a04",
+    COLOR_ILUMINACION_PESTANAS_HOME: "#ca8a04",
+    COLOR_CODIGO_VIDEO: "#eab308",
+    COLOR_BTN_VER_MAS_OCULTAR: "#eab308",
+    COLOR_NOMBRE_ACTRIZ_DESCRIPCION: "#38bdf8",
+    COLOR_ETIQUETAS_FECHAS: "#eab308",
+    COLOR_ILUMINACION_PESTANAS_ACTRIZ: "#ca8a04",
+    COLOR_NOMBRE_ACTRIZ_PAGINA: "#eab308",
+    COLOR_SUBTEXTO_ORDENAR_POR: "#9ca3af",
+    COLOR_BORDE_BUSCADOR: "#ca8a04",
+
+    // Textos de Modales
+    TITULO_MODAL_NOMBRES_ARTISTICOS: "NOMBRES ARTÍSTICOS",
+    TITULO_MODAL_GUIA_ABREVIACIONES: "Guía de Abreviaciones",
+
+    // Estilos del Botón "Entendido"
+    COLOR_TEXTO_BTN_ENTENDIDO: "#030712",
+    COLOR_FONDO_BTN_ENTENDIDO: "#ca8a04"
+};
