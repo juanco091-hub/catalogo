d0692dc25fb3eccf273b14589ab394162967236a
juanco091-hub <juanco091@gmail.com>
2026-07-24 19:51:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-const CONFIG_ENV = {
-    // Configuración de Colores e Iluminación (Formatos: hex #38bdf8, rgb, color name)
+const CONFIG_OPCIONES = {
     COLOR_ILUMINACION_BTN_CEN: "#ca8a04",
     COLOR_ILUMINACION_PESTANAS_HOME: "#ca8a04",
     COLOR_CODIGO_VIDEO: "#eab308",
@@ -11,11 +10,9 @@ const CONFIG_ENV = {
     COLOR_SUBTEXTO_ORDENAR_POR: "#9ca3af",
     COLOR_BORDE_BUSCADOR: "#ca8a04",
 
-    // Textos de Modales
     TITULO_MODAL_NOMBRES_ARTISTICOS: "NOMBRES ARTÍSTICOS",
     TITULO_MODAL_GUIA_ABREVIACIONES: "Guía de Abreviaciones",
 
-    // Estilos del Botón "Entendido"
     COLOR_TEXTO_BTN_ENTENDIDO: "#030712",
     COLOR_FONDO_BTN_ENTENDIDO: "#ca8a04"
 };
